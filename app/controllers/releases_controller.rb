416c7394ca08a6696af5fa7e0d637ed075e5866b
randydinh <randy@househappy.org>
2015-06-10 21:29:27 -0700

    Update to test suite

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -12,10 +12,11 @@ end
 
 post '/:organization/:repo/:commit_sha' do
   response = FatTag.post(params[:organization], params[:repo], params[:commit_sha])
-  puts response.body
+  status 201
 end
 
 # Source Control
 # Write Tests
 # * Unit Test
 # * Integrated Test
+# Setup ruby logging to include response body
